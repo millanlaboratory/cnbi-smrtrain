3d1658f04d40a23854e51c713e71aa548b1cff07
JulienR <rechenmann@gmail.com>
2019-03-13 10:23:20 +0100

    remove dumb path

--- smr_train_init.m
+++ smr_train_init.m
@@ -1,5 +1,6 @@
 %smr_train_root = getenv('SMR_TRAIN_ROOT');
-smr_train_root = '/home/rleeb/Git/smr_train';
+homeDir = getuserdir();
+smr_train_root = [homeDir '/dev/cnbi-smrtrain'];
 addpath('/usr/share/cnbiloop/cnbitkmat/mtpath/');
 
 addpath([smr_train_root '/classification/']);
